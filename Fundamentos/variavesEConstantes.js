9defbba0963e141166e346478abf7f81340baf51
Rafaela Marraschi <rafaela.vm@gmail.com>
2018-01-02 15:10:42 -0200

    11 - O básico de var,let e const

---
+++ Fundamentos/variavesEConstantes.js
@@ -0,0 +1,18 @@
+var a = 3; 
+
+let b = 4;
+
+var a = 30; //var pode ser declarado duas vezes
+b = 40;
+
+console.log(a,b);
+
+a = 300;
+b = 400;
+
+console.log(a,b);
+
+const c = 5;
+//c = 50; // não pode, dá erro, pois o valor é fixo
+
+console.log(c);
