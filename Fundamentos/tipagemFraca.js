4ab1af3c9d5017a2ca5b4b05c480ef82dbf37ab0
Rafaela Marraschi <rafaela.vm@gmail.com>
2018-01-02 17:14:25 -0200

    12 - Tipagem fraca

---
+++ Fundamentos/tipagemFraca.js
@@ -0,0 +1,11 @@
+let qualquer = 'legal';
+
+console.log(typeof qualquer);
+
+qualquer = 3.14;
+
+console.log(qualquer);
+
+console.log(typeof qualquer);
+
+//linguagem de tipagem fraca: a variável pode ter múltiplos valores
