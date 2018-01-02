f8d992b6ac969e7956f4d542e395747a00a138e9
Rafaela Marraschi <rafaela.vm@gmail.com>
2018-01-02 17:57:06 -0200

    14 - Numbers : alguns cuidados

---
+++ Fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,13 @@
+console.log(7/0); //o resultado dá 'Infinity'. Não seria necessariamente um erro
+
+console.log("10"/2); // ele dá o resultado normal : 5
+
+console.log("legal"/2); // NaN: não é um número. 
+
+console.log("show"*2); //NaN
+
+console.log(0.1 + 0.7);
+
+//console.log(10.toString()); //dá erro
+
+console.log((10.345).toFixed(2));
