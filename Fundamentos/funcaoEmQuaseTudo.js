3a1b3304b6e46598ae5edd0794999aca99471550
Rafaela Marraschi <rafaela.vm@gmail.com>
2018-01-10 14:47:39 -0200

    23 - Quase tudo é função

---
+++ Fundamentos/funcaoEmQuaseTudo.js
@@ -0,0 +1,7 @@
+console.log(typeof Object); //O objeto é uma função
+
+class Produto{}
+
+console.log(typeof Produto); //classe Produto também é uma função
+
+//dentro de uma função pode ter atributos, dados, uma outra função, etc
