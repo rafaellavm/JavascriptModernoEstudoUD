c189c1dbde8aa3bb3789e404c719a6526d0af62c
Rafaela Marraschi <rafaela.vm@gmail.com>
2018-01-19 15:17:39 -0200

    33 - Entendendo o hosting

---
+++ Fundamentos/hoisting.js
@@ -0,0 +1,13 @@
+console.log('a = ', a); //undefined. Dá 'undefined' mas não é considerado erro
+
+var a = 2; 
+
+console.log('a = ', a); //a
+
+//----------------------
+
+console.log('b = ', b); //dá erro
+
+let b = 2; 
+
+console.log('b = ', b); 
