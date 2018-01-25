136c8bdb4dbcd7929940824b52bca38220d7a79e
Rafaela Marraschi <rafaela.vm@gmail.com>
2018-01-25 13:30:44 -0200

    37 - Operadores atribuição

---
+++ Fundamentos/operador_atribuicao.js
@@ -0,0 +1,17 @@
+const a = 7;
+let b = 3;
+
+b += a; //acrescentando o valor de a em b
+console.log(b); //10
+
+b -= 4; // b =  b - 4;
+console.log(b); //6
+
+b *= 2 //b = b * 2
+console.log(b); //12
+
+b /= 2; // b = b / 2 = 6
+console.log(b);
+
+b %= 2; // b = b % 2 (resto da divisão) = 6/2 = 0 (resto)
+console.log(b);
